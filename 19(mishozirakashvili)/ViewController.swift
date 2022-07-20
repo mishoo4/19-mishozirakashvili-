571d96b7545f2b8d2b289275d8c568a2eca31e9c
mishoo4 <104001328+mishoo4@users.noreply.github.com>
2022-07-21 02:17:41 +0400

    Initial Commit

---
+++ 19(mishozirakashvili)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19(mishozirakashvili)
+//
+//  Created by misho zirakashvili on 21.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
